fe4173316888f3ede16ab8749a2f1244030417fa
xiangnanren <renxiangnan90@gmail.com>
2017-05-15 23:42:18 +0200

    Delete test codes

--- src/main/scala/engine/launcher/streamgenerator/Generator3.scala
+++
@@ -1,8 +0,0 @@
-package engine.launcher.streamgenerator
-
-/**
-  * Created by xiangnanren on 19/04/2017.
-  */
-case class Generator3(workLoad: Long) extends StreamGenerator {
-  override def generate(): Unit = ???
-}
