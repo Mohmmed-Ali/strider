4ceb08fb37023488b857bfe86b973c299a68f208
xiangnanren <renxiangnan90@gmail.com>
2017-05-25 04:55:39 +0800

    Project update

---
+++ src/main/scala/engine/core/sparkexpr/expr/SparkExprException.scala
@@ -0,0 +1,9 @@
+package engine.core.sparkexpr.expr
+
+/**
+  * Created by xiangnanren on 25/05/2017.
+  */
+sealed abstract class SparkExprException extends Exception
+
+case class NullExprException(msg: String) extends Exception(msg)
+case class VarOutOfBoundException(msg: String) extends Exception(msg)
--- src/main/scala/engine/core/sparkop/op/SparkFilter.scala
+++ src/main/scala/engine/core/sparkop/op/SparkFilter.scala
@@ -2,9 +2,10 @@ package engine.core.sparkop.op
 
 import engine.core.sparkexpr.compiler.SparkExprTransformer
 import engine.core.sparkexpr.executor.SparkExprExecutor
-import engine.core.sparkexpr.expr.SparkExpr
+import engine.core.sparkexpr.expr.{NullExprException, SparkExpr, VarOutOfBoundException}
 import engine.core.sparkop.compiler.SparkOpVisitor
 import org.apache.jena.sparql.algebra.op.OpFilter
+import org.apache.jena.sparql.expr.Expr
 import org.apache.spark.sql.functions.udf
 
 
@@ -15,7 +16,26 @@ import org.apache.spark.sql.functions.udf
 class SparkFilter(val opFilter: OpFilter,
                   subOp: SparkOp) extends
   SparkOp1(subOp: SparkOp) {
-  val expr = transform(opFilter)
+  val expr = opFilter.getExprs.iterator.next()
+  val columnName = setColumnName(expr)
+
+  @throws(classOf[NullExprException])
+  val transformedExpr = try {
+    (new SparkExprTransformer).transform(expr)
+  } catch {
+    case ex: Exception =>
+      throw NullExprException("The expression in" + this.opName + "is null")
+  }
+
+  @throws(classOf[VarOutOfBoundException])
+  def setColumnName(expr: Expr): String = {
+    expr.getExprVar.getVarNamesMentioned.size match {
+      case size if size > 1 =>
+        throw VarOutOfBoundException(
+          "The number of variable in an expression should be less than 1")
+      case size if size == 1  => expr.getVarName
+    }
+  }
 
   def computeExpr(expr: SparkExpr) = udf(
     (arg: Any) => {
@@ -38,7 +58,7 @@ class SparkFilter(val opFilter: OpFilter,
     val expr = opFilter.getExprs.iterator.next()
 
     (new SparkExprTransformer).transform(expr)
-  }
+}
 }
 
 
